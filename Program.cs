1e14a64061ab53549686f8ab938873ac4b5fa63c
criorredev <alexsapptx@gmail.com>
2026-01-18 03:49:54 -0600

    Add Player and Goblin classes with health points

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Player
+{
+    public int health = 20;
+}
+
+class Goblin
+{
+    public int health = 12;
+}
