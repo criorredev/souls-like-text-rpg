1d60debafe004641a39ca61ca14180d4876e7167
criorredev <alexsapptx@gmail.com>
2026-01-18 03:50:24 -0600

    Add console output for Player and Goblin HP

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
-﻿class Player
+﻿Goblin goblin = new Goblin();
+Player player = new Player();
+
+Console.WriteLine($"Goblin HP: {goblin.health}");
+Console.WriteLine($"Player HP: {player.health}");
+
+class Player
 {
     public int health = 20;
 }
